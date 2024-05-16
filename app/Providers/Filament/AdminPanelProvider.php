c213228f21b80d80fd864e0b47f227de91fff616
199ocero <199ocero@gmail.com>
2024-05-16 18:10:36 +0800

    added custom theme

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -46,6 +46,7 @@ class AdminPanelProvider extends PanelProvider
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
             ])
+            ->viteTheme('resources/css/filament/admin/theme.css')
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
---
+++ resources/css/filament/admin/tailwind.config.js
@@ -0,0 +1,11 @@
+import preset from '../../../../vendor/filament/filament/tailwind.config.preset'
+
+export default {
+    presets: [preset],
+    content: [
+        './app/Filament/Admin/**/*.php',
+        './resources/views/filament/admin/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
+        './packages/filament-monaco-editor/resources/views/**/*.blade.php'
+    ],
+}
