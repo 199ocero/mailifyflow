77e6c927d9ee5175ddb8761b00c9b1c938646543
199ocero <199ocero@gmail.com>
2024-05-23 20:23:37 +0800

    added icon

--- app/Filament/Admin/Resources/TemplateResource/Pages/ListTemplates.php
+++ app/Filament/Admin/Resources/TemplateResource/Pages/ListTemplates.php
@@ -13,7 +13,9 @@ class ListTemplates extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->label('Add New Template')
+                ->icon('heroicon-o-plus'),
         ];
     }
 }
