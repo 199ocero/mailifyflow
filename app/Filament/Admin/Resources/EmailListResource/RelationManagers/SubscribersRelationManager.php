87f1206959d1d1e12509ab2db5aba5f1e37fe78d
199ocero <199ocero@gmail.com>
2024-05-24 18:56:34 +0800

    added subscriber relation manager and importer

--- app/Filament/Admin/Resources/EmailListResource/RelationManagers/SubscribersRelationManager.php
+++ app/Filament/Admin/Resources/EmailListResource/RelationManagers/SubscribersRelationManager.php
@@ -3,12 +3,13 @@
 namespace App\Filament\Admin\Resources\EmailListResource\RelationManagers;
 
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Imports\SubscriberRelationImporter;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Resources\RelationManagers\RelationManager;
 
 class SubscribersRelationManager extends RelationManager
 {
@@ -19,9 +20,22 @@ class SubscribersRelationManager extends RelationManager
         return $form
             ->schema([
                 Forms\Components\TextInput::make('email')
+                    ->label('Email Address')
+                    ->placeholder('e.g. johndoe@gmail.com')
                     ->required()
-                    ->maxLength(255),
-            ]);
+                    ->email(),
+                Forms\Components\TextInput::make('first_name')
+                    ->label('First Name')
+                    ->placeholder('e.g. John')
+                    ->nullable()
+                    ->string(),
+                Forms\Components\TextInput::make('last_name')
+                    ->label('Last Name')
+                    ->placeholder('e.g. Doe')
+                    ->nullable()
+                    ->string(),
+            ])
+            ->columns(1);
     }
 
     public function table(Table $table): Table
@@ -29,13 +43,36 @@ class SubscribersRelationManager extends RelationManager
         return $table
             ->recordTitleAttribute('email')
             ->columns([
-                Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('first_name')
+                    ->placeholder('No First Name')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('last_name')
+                    ->placeholder('No Last Name')
+                    ->searchable()
+                    ->sortable(),
             ])
+            ->defaultSort('subscribers.created_at', 'desc')
             ->filters([
                 //
             ])
             ->headerActions([
-                Tables\Actions\CreateAction::make(),
+                Tables\Actions\CreateAction::make()
+                    ->label('Add Subscriber')
+                    ->icon('heroicon-o-plus')
+                    ->modalHeading('Add Subscriber'),
+                Tables\Actions\ImportAction::make()
+                    ->label('Import Subscribers')
+                    ->color('gray')
+                    ->icon('heroicon-o-arrow-down-tray')
+                    ->importer(SubscriberRelationImporter::class)
+                    ->modalHeading('Import Subscribers')
+                    ->options([
+                        'email_list_id' =>  $this->getOwnerRecord()->getKey(),
+                    ]),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
---
+++ app/Filament/Imports/SubscriberRelationImporter.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Imports;
+
+use App\Models\Subscriber;
+use Filament\Facades\Filament;
+use App\Enum\SubscriberStatusType;
+use App\Models\EmailList;
+use Filament\Forms;
+use Filament\Actions\Imports\Importer;
+use Filament\Actions\Imports\ImportColumn;
+use Filament\Actions\Imports\Models\Import;
+use Illuminate\Support\Arr;
+
+class SubscriberRelationImporter extends Importer
+{
+    protected static ?string $model = Subscriber::class;
+
+    public static function getOptionsFormComponents(): array
+    {
+        return [];
+    }
+
+    public static function getColumns(): array
+    {
+        return [
+            ImportColumn::make('email')
+                ->label('Email')
+                ->requiredMapping()
+                ->rules(['required', 'email', 'max:255'])
+                ->example('johndoe@gmail.com'),
+            ImportColumn::make('first_name')
+                ->label('First Name')
+                ->requiredMapping()
+                ->rules(['nullable', 'string', 'max:255'])
+                ->example('John'),
+            ImportColumn::make('last_name')
+                ->label('Last Name')
+                ->requiredMapping()
+                ->rules(['nullable', 'string', 'max:255'])
+                ->example('Doe'),
+        ];
+    }
+
+    public function resolveRecord(): ?Subscriber
+    {
+        return new Subscriber([
+            'email' => $this->data['email'],
+            'first_name' => $this->data['first_name'],
+            'last_name' => $this->data['last_name'],
+            'status' => SubscriberStatusType::SUBSCRIBED->value,
+        ]);
+    }
+
+    public static function getCompletedNotificationBody(Import $import): string
+    {
+        $body = 'Your subscriber import has completed and ' . number_format($import->successful_rows) . ' ' . str('row')->plural($import->successful_rows) . ' imported.';
+
+        if ($failedRowsCount = $import->getFailedRowsCount()) {
+            $body .= ' ' . number_format($failedRowsCount) . ' ' . str('row')->plural($failedRowsCount) . ' failed to import.';
+        }
+
+        return $body;
+    }
+
+    public function saveRecord(): void
+    {
+        // Save the subscriber instance
+        $this->record->save();
+
+        // Attach the subscriber to the email list
+        $emailList = EmailList::findOrFail($this->options['email_list_id']);
+        $emailList->subscribers()->attach($this->record->id);
+    }
+}
