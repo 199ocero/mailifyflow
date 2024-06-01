2e0067a105c966d3de11fb2ca1c41d803681dc86
199ocero <199ocero@gmail.com>
2024-06-01 15:15:07 +0800

    added send job

---
+++ app/Jobs/Ses/SendJob.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Jobs\Ses;
+
+use App\Enum\CampaignLogStatusType;
+use App\Models\CampaignEmail;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Spatie\WebhookClient\Models\WebhookCall;
+
+class SendJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public WebhookCall $webhookCall)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $message = $this->webhookCall->payload['Message'];
+
+        $campaignId = null;
+        $subscriberId = null;
+
+        foreach ($message['mail']['headers'] as $header) {
+            if ($header['name'] === 'Campaign-Id') {
+                $campaignId = $header['value'];
+                break;
+            }
+        }
+
+        foreach ($message['mail']['headers'] as $header) {
+            if ($header['name'] === 'Subscriber-Id') {
+                $subscriberId = $header['value'];
+                break;
+            }
+        }
+
+        $campaignLog = CampaignEmail::query()
+            ->with('subscriber')
+            ->where('campaign_id', $campaignId)
+            ->where('subscriber_id', $subscriberId)
+            ->first();
+
+        if ($campaignLog) {
+            $campaignLog->status = CampaignLogStatusType::SENT->value;
+            $campaignLog->sent_at = Carbon::parse($message['mail']['timestamp']);
+            $campaignLog->save();
+        }
+    }
+}
